e410d69f30ec15f54fd8d50914a60fd66d30f878
Hiroto.i <hiroto.iizuka@naviplus.co.jp>
2024-07-31 09:33:20 +0900

    Ruby3.0に対応

--- lib/cequel/version.rb
+++ lib/cequel/version.rb
@@ -1,5 +1,7 @@
 # -*- encoding : utf-8 -*-
 module Cequel
   # The current version of the library
-  VERSION = '3.2.1'
+  # フォークしてパッチ対応しているため、バージョンを変更
+  NP_REVISION = '1'.freeze
+  VERSION = "3.2.1.#{NP_REVISION}".freeze
 end
